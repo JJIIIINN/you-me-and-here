d7d841a677ebcd2e7658ba29e8e537cc6fbb9256
JJIIIINN <kgb21537@gmail.com>
2023-12-02 01:39:47 +0900

    feat: 개발 완료

---
+++ src/components/main/index.js
@@ -0,0 +1,195 @@
+import React, { useEffect, useState } from "react";
+import Here from "../../assets/svg/Here.svg";
+import { Map, MapMarker, MarkerClusterer } from "react-kakao-maps-sdk";
+import styled from "styled-components";
+import { getPlaceMarkerImg, randomMarkerImg } from "../../utils/getMarkerImg";
+import { shareKakao } from "../../utils/shareKakao";
+
+export default function MainPage({ setMap, map, contents, coordArray }) {
+    const { kakao } = window;
+    const [openInfo, setOpenInfo] = useState(0);
+    const [viewMarker, setViewMarker] = useState(false);
+    const [markers, setMarkers] = useState([]);
+    const [location, setLoacation] = useState({
+        lat: 0,
+        lng: 0,
+    }); // 현재 위치를 저장할 상태
+
+    useEffect(() => {
+        successHandler(location); // 성공시 successHandler, 실패시 errorHandler 함수가 실행된다.
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [map, contents]);
+
+    useEffect(() => {
+        if (!!coordArray.length) {
+            setLoacation({
+                lat:
+                    coordArray
+                        .map((item) => Number(item.y))
+                        .reduce((yCoord1, yCoord2) => yCoord1 + yCoord2) /
+                    coordArray.length,
+                lng:
+                    coordArray
+                        .map((item) => Number(item.x))
+                        .reduce((xCoord1, xCoord2) => xCoord1 + xCoord2) /
+                    coordArray.length,
+            });
+            setViewMarker(true);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [coordArray]);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((response) => {
+            const { latitude, longitude } = response.coords;
+            setLoacation({ lat: latitude, lng: longitude });
+        }); // 성공시 successHandler, 실패시 errorHandler 함수가 실행된다.
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const successHandler = (response) => {
+        if (!contents) return;
+        const ps = new kakao.maps.services.Places();
+        ps.keywordSearch(
+            // 음식점 - 한식 / 중식 / 양식 / 일식
+            // 카페 - 보드게임카페 / 카페
+            // 스포츠 - 당구장 / 볼링장 / 탁구장
+            // 여가 - pc방 / 노래방
+            // 숙박업소 - 호텔 / 모텔 / 게스트하우스
+            contents,
+            (data, _pagination) => {
+                setMarkers(data);
+            },
+            {
+                location: new kakao.maps.LatLng(response.lat, response.lng),
+                radius: 1500,
+            }
+        );
+    };
+
+    return (
+        <Map
+            center={location} // 지도의 중심 좌표
+            style={{ width: "100vw", height: "100vh" }} // 지도 크기
+            level={5} // 지도 확대 레벨
+            onCreate={setMap}
+            key={[markers, coordArray, location, contents]}
+        >
+            <MarkerClusterer averageCenter={true} minLevel={6}>
+                {markers.map((marker) => {
+                    return (
+                        <MapMarker
+                            key={`marker2-${marker.x},${marker.y}`}
+                            image={{
+                                src: getPlaceMarkerImg(marker.category_name),
+                                size: { width: 60, height: 60 },
+                            }}
+                            position={{ lat: marker.y, lng: marker.x }}
+                            onClick={() =>
+                                openInfo === marker.id
+                                    ? setOpenInfo(0)
+                                    : setOpenInfo(marker.id)
+                            }
+                        >
+                            {openInfo === marker.id && (
+                                <TextBox>
+                                    {marker.place_name}
+                                    <LinkBox>
+                                        <div
+                                            style={{ cursor: "pointer" }}
+                                            onClick={() =>
+                                                shareKakao(
+                                                    marker.place_url,
+                                                    marker.place_name,
+                                                    marker.category_name
+                                                )
+                                            }
+                                        >
+                                            공유하기
+                                        </div>
+                                        •
+                                        <a
+                                            style={{ cursor: "pointer" }}
+                                            href={`https://map.kakao.com/link/to/${marker.id}`}
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            길찾기
+                                        </a>
+                                    </LinkBox>
+                                </TextBox>
+                            )}
+                        </MapMarker>
+                    );
+                })}
+                {coordArray?.map((juso, i) => {
+                    return (
+                        <MapMarker
+                            key={`marker1-${juso.y},${juso.x}`}
+                            image={{
+                                src: randomMarkerImg(i),
+                                size: { width: 60, height: 60 },
+                            }}
+                            position={{ lat: juso.y, lng: juso.x }}
+                            onMouseOver={
+                                // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
+                                () => setOpenInfo(juso.address.address_name)
+                            }
+                            // 마커에 마우스아웃 이벤트를 등록합니다
+                            onMouseOut={
+                                // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
+                                () => setOpenInfo(0)
+                            }
+                        >
+                            {openInfo === juso.address.address_name && (
+                                <TextBox>
+                                    {juso.road_address.building_name
+                                        ? juso.road_address.building_name
+                                        : juso.address.address_name}
+                                </TextBox>
+                            )}
+                        </MapMarker>
+                    );
+                })}
+            </MarkerClusterer>
+            {viewMarker && (
+                <MapMarker
+                    image={{
+                        src: Here,
+                        size: { width: 60, height: 60 },
+                    }}
+                    position={location}
+                    onMouseOver={
+                        // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
+                        () => setOpenInfo("Here")
+                    }
+                    // 마커에 마우스아웃 이벤트를 등록합니다
+                    onMouseOut={
+                        // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
+                        () => setOpenInfo(0)
+                    }
+                />
+            )}
+        </Map>
+    );
+}
+
+const TextBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 10px 0;
+    align-items: center;
+    justify-content: center;
+    width: 200px;
+    padding: 10px;
+    font-size: 16px;
+    font-weight: 700;
+`;
+
+const LinkBox = styled.div`
+    margin-top: 10px;
+    font-size: 12px;
+    font-weight: 400;
+    color: #3d75cc;
+    display: flex;
+`;
